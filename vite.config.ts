cdb9c3c2a6ad5f7c380e94cbf95fb8265a19fa09
Hung Truong <hungtruongquoc@gmail.com>
2025-04-08 23:32:31 -0500

    Added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Sitemap from 'vite-plugin-sitemap'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    tailwindcss(),
     Sitemap({
       hostname: 'https://work-well.net',
       // For dynamic routes
